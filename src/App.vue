dcad6cbb65d7febe95d3df9a2d5cccda35a44f21
KatiaLosignore <katialos.kl@gmail.com>
2023-05-30 18:38:07 +0200

    added Milestone 5

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script>
   import AppHeader from './components/AppHeader.vue';
+  import ProjectCard from './components/ProjectCard.vue';
   import axios from 'axios';
 
   export default {
-    components: { AppHeader },
+    components: { AppHeader , ProjectCard},
 
     data() {
       return {
@@ -23,12 +24,6 @@
          this.projects = response.data.results;
         })
       },
-      truncateText(text) {
-        if (text && text.length > this.contentMaxLength) {
-        return text.substr(0, this.contentMaxLength) + '...';
-        }
-        return text;
-      }
     },
     mounted() {
       this.getProjects();
@@ -40,29 +35,10 @@
 </script>
 
 <template>
+  
   <AppHeader></AppHeader>
-  
-   <div class="container">
-        <div class="row">
-
-            <div class="col-4" v-for="project in projects">
-                <div class="card">
-                    <img v-if="project.image" :src="`${this.baseUrl}/storage/${project.image}`" class="card-img-top">
-                    <img v-else src="https://cdn.icon-icons.com/icons2/1462/PNG/512/120nophoto_100007.png" class="card-img-top"/>
-                    <div class="card-body">
-                        <h5 class="card-title">{{project.title}}</h5>
-                        <h5>{{project.type?.name}}</h5>
-                        <p class="card-text">{{truncateText(project.content)}}</p>
-                      
-                    </div>
-                </div>
-            </div>
-
-           
-            
-        </div>
-    </div>
-  
+  <ProjectCard :cards="projects"></ProjectCard>
+   
 </template>
 
 <style lang="scss">
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,55 @@
+<script>
+
+export default {
+    name: 'ProjectCard',
+
+    data() {
+        return {
+            baseUrl: 'http://localhost:8000',
+        }
+    },
+    props: {
+        cards: Array
+    },
+    methods: {
+        truncateText(text) {
+        if (text && text.length > this.contentMaxLength) {
+        return text.substr(0, this.contentMaxLength) + '...';
+        }
+        return text;
+      }
+    }
+};
+
+</script>
+
+
+
+
+
+
+<template>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-4" v-for="card in cards">
+                <div class="card">
+                    <img v-if="card.image" :src="`${this.baseUrl}/storage/${card.image}`" class="card-img-top">
+                    <img v-else src="https://cdn.icon-icons.com/icons2/1462/PNG/512/120nophoto_100007.png" class="card-img-top"/>
+                    <div class="card-body">
+                        <h5 class="card-title">{{card.title}}</h5>
+                        <h5>{{card.type?.name}}</h5>
+                        <p class="card-text">{{truncateText(card.content)}}</p>
+                      
+                    </div>
+                </div>
+            </div>
+
+           
+            
+        </div>
+    </div>
+  
+
+</template>
