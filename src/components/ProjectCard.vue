ce75a887ce488f7c64e7ffc5a679634b4257bb14
KatiaLosignore <katialos.kl@gmail.com>
2023-05-30 19:50:09 +0200

    added bonus with pagination

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -26,16 +26,13 @@ export default {
 
 
 
-
-
-
 <template>
 
     <div class="container">
         <div class="row">
 
             <div class="col-4" v-for="card in cards">
-                <div class="card">
+                <div class="card mt-5">
                     <img v-if="card.image" :src="`${this.baseUrl}/storage/${card.image}`" class="card-img-top">
                     <img v-else src="https://cdn.icon-icons.com/icons2/1462/PNG/512/120nophoto_100007.png" class="card-img-top"/>
                     <div class="card-body">
@@ -45,12 +42,8 @@ export default {
                       
                     </div>
                 </div>
-            </div>
-
-           
-            
+            </div>                      
         </div>
-    </div>
-  
+    </div>    
 
 </template>
